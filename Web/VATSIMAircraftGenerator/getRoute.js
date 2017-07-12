e59ce64b3c8cafb51081e6d181989cea80ed7ade
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-07-11 21:06:55 -0400

    Moved VATSIM ACS Generator repo to https://github.com/demilletech/vatsimacsgenerator

--- Web/VATSIMAircraftGenerator/getRoute.js
+++
@@ -1,9 +0,0 @@
-function getRoute(from, to) {
-    for (var i = 0; i < routes.length; i++) {
-        var item = routes[i];
-        if (item[0] == from && item[2] == to) {
-            return item;
-        }
-    }
-    return null;
-}
