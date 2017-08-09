7860e5028492d4fc2e264e0f5bc38bc4e4b3f0c0
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-08-08 23:29:15 -0400

    Added firewall commands for mysql

--- Scripts/cos7-mysql.sh
+++ Scripts/cos7-mysql.sh
@@ -4,5 +4,7 @@ cd mysql
 wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 rpm -ivh mysql57-community-release-el7-9.noarch.rpm
 sudo yum install mysql-server -y
+firewall-cmd --permanent --add-service=mysql
+firewall-cmd --reload
 sudo systemctl start mysqld
 grep 'temporary password' /var/log/mysqld.log
