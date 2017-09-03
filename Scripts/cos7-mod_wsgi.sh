929b40b8ec3156a50d4e2b936ddfcd55caaf7db5
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-09-02 23:35:32 -0400

    Create cos7-mod_wsgi.sh

---
+++ Scripts/cos7-mod_wsgi.sh
@@ -0,0 +1,10 @@
+yum -y install gcc httpd-devel
+export mod_wsgi_github_dl_url=`curl -s https://api.github.com/repos/GrahamDumpleton/mod_wsgi/releases/latest | grep tarball_url | cut -d '"' -f 4`
+wget -O mod_wsgi.tar.gz $mod_wsgi_github_dl_url
+mkdir mod_wsgi
+tar -xvf mod_wsgi.tar.gz -C mod_wsgi
+cd mod_wsgi
+cd *
+./configure --with-apxs=/usr/bin/apxs --with-python=/usr/bin/python3
+make
+make install
