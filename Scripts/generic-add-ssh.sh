b2490102e110d01912fa1574a0648984991b754b
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-09-10 20:34:40 -0400

    Update generic-add-ssh.sh

--- Scripts/generic-add-ssh.sh
+++ Scripts/generic-add-ssh.sh
@@ -1,3 +1,5 @@
+mkdir ~/.ssh
+
 echo "# Laptop's SSH key\nssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMx0wR5JQ+04QcjBRsckG1kxrRaq0+vcjlnREBYZPP7SdYqGRtiZYtEZxiFlfCSF626PXITBrz1XhkZrbbLj8bdYmtWDg+IR7HJJLuHmFgh/HJlRn7jDqQ3Q8W1Ay6tq1n1fINSnuuuDhi+sWtR4gilYngk5O9JyKbs7mDIckaqHrWLhs9oWc8ZcMADy0oaWDyGU28Z1wf6Fz+zB4Np3hsdxs1HOHKH77y2go3Q709BlYwnRKW8hta19Nbn+eBiwWMGHqCXydOW3Yp3EViQ4BaAtDWOmXy1KNKy+EdTad0JKxVkYeSX+MiJSBLYCM4nuhzoTzZrfOvwdIAxlI+OABJ space@Eamonns-Air" >> ~/.ssh/authorized_keys
 
 echo "# Desktop's PUTTY key\nssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAzI5xAFGGgM8HFn5o+ItZHJNryIsqayrKcD5RsGeyGTA3nXH6fk0WsiVADOjPN/GEry/72d6uK9bUjZctpx54K2caxPwf93Md3MBS99fwrQcjwM/7DAVen/w8XTs1w+YMCcxGPXfYU2PfFCukzWVvXwPcFGgiXGrMPmgRp2yqQz2vosD/DXioRtD1h80PRdDN3Xbs1+XS0J6vZzF3Ffv7zLGporxgLgCpv74N7V7Xt6IjM2PyLQ5TJU79Z/l8gbzlwoOh+UxC9qb+lUONraRdyYmG/AYPdcfJ0IJZru0kZBY0j8yeyJAX40FPgKPBgB3FGPqQtFTfGlK5KtvVo7qH+Q== rsa-key-20161023" >> ~/.ssh/authorized_keys
