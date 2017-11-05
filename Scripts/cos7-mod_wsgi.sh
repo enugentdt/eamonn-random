90b4563efb43f003e6d87152d2c8227a9721b434
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-11-04 20:37:00 -0400

    Update cos7-mod_wsgi.sh

--- Scripts/cos7-mod_wsgi.sh
+++ Scripts/cos7-mod_wsgi.sh
@@ -1,3 +1,4 @@
+cd ~
 yum -y install gcc httpd-devel mod_wsgi
 export mod_wsgi_github_dl_url=`curl -s https://api.github.com/repos/GrahamDumpleton/mod_wsgi/releases/latest | grep tarball_url | cut -d '"' -f 4`
 wget -O mod_wsgi.tar.gz $mod_wsgi_github_dl_url
