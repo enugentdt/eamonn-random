72dda72632108f629d9e964a7179c590964ccee9
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-11-24 18:26:01 -0500

    Update cos7-nagios-add-remote-host.sh

--- Scripts/cos7-nagios-add-remote-host.sh
+++ Scripts/cos7-nagios-add-remote-host.sh
@@ -1,7 +1,7 @@
 yum -y install epel-release
 yum -y install nrpe nagios-plugins-all
 
-sed 's/allowed_hosts=127.0.0.1,::1/allowed_hosts=127.0.0.1,::1,10.101.22.50/w' /etc/nagios/nrpe.cfg
-
 systemctl start nrpe.service
 systemctl enable nrpe.service
+
+sed 's/allowed_hosts=127.0.0.1,::1/allowed_hosts=127.0.0.1,::1,10.101.22.50/w' /etc/nagios/nrpe.cfg
