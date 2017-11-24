e8723b28de3ee332687cc437f4fbc8f7ff99e5e0
Eamonn Nugent <eamonn.nugent@demilletech.net>
2017-11-24 01:31:02 -0500

    Update cos7-nagios-add-remote-host.sh

--- Scripts/cos7-nagios-add-remote-host.sh
+++ Scripts/cos7-nagios-add-remote-host.sh
@@ -1,5 +1,5 @@
-yum install epel-release
-yum install nrpe nagios-plugins-all
+yum -y install epel-release
+yum -y install nrpe nagios-plugins-all
 
 sed 's/allowed_hosts=127.0.0.1,::1/allowed_hosts=127.0.0.1,::1,10.101.22.50/w' /etc/nagios/nrpe.cfg
 
