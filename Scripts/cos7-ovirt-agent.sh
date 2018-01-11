5c15158311b9f329a50c052cc43983fd5d43972e
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-01-11 13:16:29 -0500

    Update cos7-ovirt-agent.sh

--- Scripts/cos7-ovirt-agent.sh
+++ Scripts/cos7-ovirt-agent.sh
@@ -1,3 +1,4 @@
-yum install ovirt-guest-agent-common
+yum -y install epel-release
+yum -y install ovirt-guest-agent-common
 systemctl enable ovirt-guest-agent.service
 systemctl start ovirt-guest-agent.service
