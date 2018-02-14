81d8c97aca9cd062282da7417f3346e86efcfc24
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-02-13 19:16:52 -0500

    Create cos7-elasticsearch.sh

---
+++ Scripts/cos7-elasticsearch.sh
@@ -0,0 +1,14 @@
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+echo "[elasticsearch-6.x]
+name=Elasticsearch repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md" >> /etc/yum.repos.d/elasticsearch.repo
+
+yum -y install elasticsearch
+firewall-cmd --permanent --add-service=elasticsearch
+firewall-cmd --reload
