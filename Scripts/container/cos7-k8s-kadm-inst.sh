95fe4ce0ba67823e4d555a1ee0ac45bd5c148d06
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-03-27 13:07:45 -0400

    Create cos7-k8s-kadm-inst.sh

---
+++ Scripts/container/cos7-k8s-kadm-inst.sh
@@ -0,0 +1,21 @@
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd --reload
+
+yum install -y docker
+systemctl enable docker && systemctl start docker
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+setenforce 0
+yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl start kubelet
