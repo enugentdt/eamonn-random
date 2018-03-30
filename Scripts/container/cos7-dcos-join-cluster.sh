038d393ed53f927d7868115f3bfd3cf3545dfdff
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-03-30 00:34:07 -0400

    Update cos7-dcos-join-cluster.sh

--- Scripts/container/cos7-dcos-join-cluster.sh
+++ Scripts/container/cos7-dcos-join-cluster.sh
@@ -16,6 +16,9 @@ fi
 
 hostnamectl set-hostname $HOSTNAME
 
+setenforce 0
+sed -i -e 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+
 sudo mkdir -p /opt/dcos_install_tmp
 sudo tar xf dcos-install.tar -C /opt/dcos_install_tmp
 sudo bash /opt/dcos_install_tmp/dcos_install.sh $TYPE
