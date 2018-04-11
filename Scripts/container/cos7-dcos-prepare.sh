37cfff09b2ffc5cb4cdae77e40fb3d1a7254d3da
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-04-10 20:25:33 -0400

    Create cos7-dcos-prepare.sh

---
+++ Scripts/container/cos7-dcos-prepare.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# This is to be run BEFORE the CLI, and does NOT install DCOS for you. This will also run my oVirt Starter Package stuff
+
+TYPE=$1
+HOSTNAME=$2
+
+if [ -z "$TYPE" ]
+then
+  echo "Command incorrect. Usage: (script) <TYPE> <HOSTNAME>"
+  exit 1
+fi
+
+if [ -z "$HOSTNAME" ]
+then
+  echo "Command incorrect. Usage: (script) <TYPE> <HOSTNAME>"
+  exit 1
+fi
+
+hostnamectl set-hostname $HOSTNAME
+
+setenforce 0
+sed -i -e 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl mask firewalld
+
+yum -y install docker
+systemctl start docker
+systemctl enable docker
+
+bash -c "$(curl -s https://raw.githubusercontent.com/enugentdt/eamonn-random/master/Scripts/mgmt/cos7-ovirt-agent.sh)"
+bash -c "$(curl -s https://raw.githubusercontent.com/enugentdt/eamonn-random/master/Scripts/mgmt/cos7-first.sh)"
+bash -c "$(curl -s https://raw.githubusercontent.com/enugentdt/eamonn-random/master/Scripts/mgmt/generic-add-ssh.sh)"
+
+groupadd nogroup
+groupadd docker # Fun fact: if you don't have this line, the install will mess up, and you *will* have to reimage the machine.
+
+echo "Done"
