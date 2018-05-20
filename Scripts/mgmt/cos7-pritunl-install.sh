215eecae7c4bd959bc6e66e59d753f0c5a20409f
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-05-20 18:15:07 -0400

    Create cos7-pritunl-install.sh

---
+++ Scripts/mgmt/cos7-pritunl-install.sh
@@ -0,0 +1,14 @@
+sudo tee -a /etc/yum.repos.d/pritunl.repo << EOF
+[pritunl]
+name=Pritunl Repository
+baseurl=https://repo.pritunl.com/stable/yum/centos/7/
+gpgcheck=1
+enabled=1
+EOF
+
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
+sudo yum -y install pritunl
+sudo systemctl start pritunl
+sudo systemctl enable pritunl
