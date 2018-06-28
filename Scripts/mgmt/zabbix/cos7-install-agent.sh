85b3e3d501599a22ef873a5bf4f587fe703be531
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-06-28 12:50:16 -0400

    Update cos7-install-agent.sh

--- Scripts/mgmt/zabbix/cos7-install-agent.sh
+++ Scripts/mgmt/zabbix/cos7-install-agent.sh
@@ -2,7 +2,7 @@ rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el
 yum -y install zabbix-agent
 export HOSTNAME=$(hostname)
 curl https://raw.githubusercontent.com/enugentdt/eamonn-random/master/Scripts/mgmt/zabbix/zabbix_agentd.conf -o /etc/zabbix/zabbix_agentd.conf
-sed -i '' 's/Hostname=zabbix-hostname/Hostname=$HOSTNAME' /etc/zabbix/zabbix_agentd.conf
+sed -i '' 's/Hostname=zabbix-hostname/Hostname=$HOSTNAME/' /etc/zabbix/zabbix_agentd.conf
 systemctl start zabbix-agent
 systemctl enable zabbix-agent
 firewall-cmd --permanent --add-port=10050/tcp
