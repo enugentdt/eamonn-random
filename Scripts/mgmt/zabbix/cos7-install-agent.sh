f7262a756730aa21bef2235e80ad04ef1bf0a33a
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-06-28 12:12:52 -0400

    Update cos7-install-agent.sh

--- Scripts/mgmt/zabbix/cos7-install-agent.sh
+++ Scripts/mgmt/zabbix/cos7-install-agent.sh
@@ -1,5 +1,5 @@
 rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
-yum install zabbix-agent
+yum -y install zabbix-agent
 systemctl start zabbix-agent
 systemctl enable zabbix-agent
 firewall-cmd --permanent --add-port=10050/tcp
