388599bc73acbbe252d71557621d4b9f828bd42d
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-09-12 19:38:41 -0400

    Update cos7-first.sh

--- Scripts/mgmt/cos7-first.sh
+++ Scripts/mgmt/cos7-first.sh
@@ -5,3 +5,4 @@ firewall-cmd --permanent --add-service=cockpit
 firewall-cmd --reload
 systemctl start cockpit
 systemctl enable cockpit
+echo "$((RANDOM % 60)) $((RANDOM % 24)) * * $((RANDOM % 7)) root yum -y upgrade; yum -y update" >> /etc/crontab
