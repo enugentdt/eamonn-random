61e6285343e5dced864fa2c5d3699f78584b5619
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-09-12 19:40:50 -0400

    Create cos7-vmware-tools.sh

---
+++ Scripts/mgmt/cos7-vmware-tools.sh
@@ -0,0 +1,5 @@
+yum -y install open-vm-tools
+tput setaf 1; echo "
+############################################################
+# This machine must be rebooted for changes to take effect #
+############################################################"
