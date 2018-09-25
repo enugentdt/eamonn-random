6a1a1b5bf75f394b4c8627c11d31d62e750e292c
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-09-24 22:52:10 -0400

    Update cos7-vmware-tools.sh

--- Scripts/mgmt/cos7-vmware-tools.sh
+++ Scripts/mgmt/cos7-vmware-tools.sh
@@ -2,4 +2,4 @@ yum -y install open-vm-tools
 tput setaf 1; echo "
 ############################################################
 # This machine must be rebooted for changes to take effect #
-############################################################"; tput tput sgr0
+############################################################"; tput sgr0
