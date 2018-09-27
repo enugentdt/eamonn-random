5edd77476318dc9273853e6e1190dd18275a461b
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-09-26 20:46:07 -0400

    Update cos7-ovirt-to-vmware.sh

--- Scripts/mgmt/cos7-ovirt-to-vmware.sh
+++ Scripts/mgmt/cos7-ovirt-to-vmware.sh
@@ -1,2 +1,4 @@
+systemctl stop ovirt-guest-agent.service
+systemctl disable ovirt-guest-agent.service
 yum -y remove ovirt-guest-agent-common
 bash -c "$(curl -s https://raw.githubusercontent.com/enugentdt/eamonn-random/master/Scripts/mgmt/cos7-vmware-tools.sh)";
