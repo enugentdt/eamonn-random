9a993f1fda74ed5826ccb9a601c1f41eb424bbc4
Eamonn Nugent <eamonn.nugent@demilletech.net>
2018-11-19 18:35:25 -0500

    Update deb9-first.sh

--- Scripts/mgmt/deb9-first.sh
+++ Scripts/mgmt/deb9-first.sh
@@ -1,2 +1,2 @@
-apt install wget curl zip unzip git dnsutils -y
+apt install wget curl zip unzip git dnsutils vim -y
 echo "$((RANDOM % 60)) $((RANDOM % 24)) * * $((RANDOM % 7)) root apt -y upgrade; apt -y update" >> /etc/crontab
