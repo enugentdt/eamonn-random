b3245f1426d48247250e9ed18bb3ed82efe138c9
Eamonn Nugent <eamonn.nugent@demilletech.net>
2019-01-01 16:54:02 -0700

    Update cos7-install-agent.sh

--- Scripts/mgmt/zabbix/cos7-install-agent.sh
+++ Scripts/mgmt/zabbix/cos7-install-agent.sh
@@ -1,4 +1,4 @@
-rpm -ivh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
 yum -y install zabbix-agent
 export HOSTNAME=$(hostname)
 curl https://raw.githubusercontent.com/enugentdt/eamonn-random/master/Scripts/mgmt/zabbix/zabbix_agentd.conf -o /etc/zabbix/zabbix_agentd.conf
