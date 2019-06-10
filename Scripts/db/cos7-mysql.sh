54c23c36aab7b2e182ad079efdf982b67dca76a2
Eamonn Nugent <eamonn.nugent@demilletech.net>
2019-06-10 01:51:41 -0400

    Update cos7-mysql.sh

--- Scripts/db/cos7-mysql.sh
+++ Scripts/db/cos7-mysql.sh
@@ -1,8 +1,4 @@
-cd ~
-mkdir mysql
-cd mysql
-wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
-rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 sudo yum install mysql-server -y
 firewall-cmd --permanent --add-service=mysql
 firewall-cmd --reload
